eeefdb6e770d7d8367d2fcf82eb234bddd6c76f8
gencys <43763900+gencys@users.noreply.github.com>
2023-05-10 23:59:25 +0100

    further improve main page

--- script.js
+++ script.js
@@ -1,59 +1,71 @@
 var $window = $(window);
 const $centeredContent = $(".centeredContent");
+const $arrow = $(".arrow-down");
 const $faceDiv = $(".faceDiv");
 const $myFace = $(".myFace");
 const $jean = $("#jean");
 var thresholdArray = [];
 var step = 0.02;
 
-for (let i = 0; i <=  1.00; i = i + step) {
-  thresholdArray.push(i);
+for (let i = 0; i <= 1.00; i = i + step) {
+	thresholdArray.push(i);
 }
 
-$(document).ready(function() {
-  $jean.mouseover(function() {
-    $faceDiv.show();
-    $myFace.attr("src", "bin/ppCropped.jpg");
-  });
+$(document).ready(function () {
+	$jean.mouseover(function () {
+		$faceDiv.show();
+		$myFace.attr("src", "bin/ppCropped.jpg");
+	});
 
-  $jean.mouseleave(function() {
-    $faceDiv.hide();
-    $myFace.attr("src", null);
-  });
+	$jean.mouseleave(function () {
+		$faceDiv.hide();
+		$myFace.attr("src", null);
+	});
 });
 
-$(document).mousemove(function(e) {
-  $faceDiv.offset({
-    left: e.pageX + 20,
-    top: e.pageY
-  });
+$(document).mousemove(function (e) {
+	$faceDiv.offset({
+		left: e.pageX + 20,
+		top: e.pageY
+	});
 });
 
 function scrollTrigger(selector, options = {}) {
-  let els = document.querySelectorAll(selector)
-  els = Array.from(els)
-  els.forEach(el => {
-    addObserver(el, options)
-  })
+	let els = document.querySelectorAll(selector)
+	els = Array.from(els)
+	els.forEach(el => {
+		addObserver(el, options)
+	})
 }
 
 function addObserver(el, options = {}) {
-  $(el).css('opacity', 0);
-  let observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-      let elem = $(entry.target);
-      if(entry.isIntersecting) {
-        x = entry.intersectionRatio;
-        elem.css('opacity', x);
-      } else {
-        elem.css('opacity', 0);
-      }
-    })
-  }, options)
-  observer.observe(el)
+	$(el).css('opacity', 0);
+	let observer = new IntersectionObserver((entries, observer) => {
+		entries.forEach(entry => {
+			let elem = $(entry.target);
+			if (entry.isIntersecting) {
+				x = entry.intersectionRatio;
+				elem.css('opacity', x);
+			} else {
+				elem.css('opacity', 0);
+				if (options.forget) {
+					observer.unobserve(el);
+				}
+			}
+		})
+	}, options)
+	observer.observe(el)
 }
 
 scrollTrigger('.mainText', {
-  rootMargin: '-30% 0 -30% 0',
-  threshold: thresholdArray
-})
+	rootMargin: '-30% 0 -30% 0',
+	threshold: thresholdArray,
+	forget: false
+});
+
+var marginTop = - $window.height() + 160;
+scrollTrigger('.arrow-wrapper', {
+	rootMargin: marginTop.toString() + 'px 0 0 0',
+	threshold: thresholdArray,
+	forget: true
+});
