5d227900972c969ad9840208406a19155a136763
gencys <43763900+gencys@users.noreply.github.com>
2023-05-13 22:47:46 +0100

    GEN-2: main work done

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ var $window = $(window);
 const $centeredContent = $(".centeredContent");
 const $arrow = $(".arrow-down");
 const $faceDiv = $(".faceDiv");
-const $myFace = $(".myFace");
 const $jean = $("#jean");
+const $qwitchLink = $("#qwitchLink");
+const $qwitchCard = $(".qwitchCard");
+const $MBLink = $("#MBLink");
+const $MBCard = $(".MBCard");
 var thresholdArray = [];
 var step = 0.02;
 
@@ -11,23 +14,29 @@ for (let i = 0; i <= 1.00; i = i + step) {
 	thresholdArray.push(i);
 }
 
+function mouseOverHandler(triggerEl, showedEl){
+	triggerEl.mouseover(function () {
+		showedEl.show();
+		//$myFace.attr("src", "bin/ppCropped.jpg");
+		$(document).mousemove(function (e) {
+			showedEl.offset({
+				left: e.pageX + 20,
+				top: e.pageY
+			});
+		});
+	});
+
+	triggerEl.mouseleave(function () {
+		showedEl.hide();
+		//$myFace.attr("src", null);
+		$(document).off("mousemove");
+	});
+}
+
 $(document).ready(function () {
-	$jean.mouseover(function () {
-		$faceDiv.show();
-		$myFace.attr("src", "bin/ppCropped.jpg");
-	});
-
-	$jean.mouseleave(function () {
-		$faceDiv.hide();
-		$myFace.attr("src", null);
-	});
-});
-
-$(document).mousemove(function (e) {
-	$faceDiv.offset({
-		left: e.pageX + 20,
-		top: e.pageY
-	});
+	mouseOverHandler($jean, $faceDiv);
+	mouseOverHandler($qwitchLink, $qwitchCard);
+	mouseOverHandler($MBLink, $MBCard);
 });
 
 function scrollTrigger(selector, options = {}) {
